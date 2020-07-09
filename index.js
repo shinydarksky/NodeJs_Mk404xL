506ba2d4915e0e9c95f5d7bfb03c4df2544393b8
MK404xL <shinydark.sky@gmail.com>
2020-07-09 21:06:40 +0700

    fix / users

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // var users = db.get("list").value()
+app.get('/',(req,res) => res.send("hello"))
 app.use('/users',useRouter)
 app.listen(port,()=>{console.log(`exemple about port ${port}`) })
 
